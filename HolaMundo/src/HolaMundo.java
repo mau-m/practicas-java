b42aef6a442f181c07424922dbdd9e2a1aa7540d
Mauricio <mirandabmau@gmail.com>
2025-10-20 22:00:06 -0600

    Ejemplos de variables

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -1,5 +1,22 @@
+import java.util.Locale;
+
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo desde Java!");
+
+        String saludo = "Hola Mundo desde Java!";
+        System.out.println(saludo);
+        System.out.println("saludo.toUpperCase() = " + saludo.toUpperCase());
+
+        int numero = 10;
+
+        boolean valor = true;
+        int numero2 = 5;
+        if(valor) {
+            System.out.println("numero = " + numero);
+            numero2 = 10;
+        }
+        System.out.println("numero2 = " + numero2);
+
+        var numero3 = "15";
     }
 }
